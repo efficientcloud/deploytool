ce67c65ee58d15dec81d0cc0bea96fca36cba9a3
Lukas Fittl <l.fittl@efficientcloud.com>
2011-07-07 17:49:17 +0200

    Graceful error if we don't know the provider.

--- lib/deploytool/command.rb
+++ lib/deploytool/command.rb
@@ -15,7 +15,13 @@ class DeployTool::Command
       puts "Deploy the current directory to the target:"
       puts "  deploy to production"
     elsif command == "add"
-      DeployTool::Config[args[0]] = DeployTool::Target.find(args[1]).to_h
+      unless target = DeployTool::Target.find(args[1])
+        puts "ERROR: Couldn't find provider for target \"#{args[1]}\""
+        puts ""
+        puts "Use \"deploy help\" if you're lost."
+        exit
+      end
+      DeployTool::Config[args[0]] = target.to_h
     elsif command == "list"
       puts "Registered Targets:"
       DeployTool::Config.all.each do |target_name, target|
@@ -26,8 +32,9 @@ class DeployTool::Command
       target_name = command == "to" ? args[0] : command
       
       unless (target = DeployTool::Config[target_name]) && !target.nil? && target.size > 0
-        puts "Couldn't find target: #{target_name}"
-        puts "Use --help if you're lost."
+        puts "ERROR: Couldn't find target: #{target_name}"
+        puts ""
+        puts "Use \"deploy help\" if you're lost."
         exit
       end
       
